df1a6363a2b4aa41f8e16d926130ae0a41009ef7
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-27 17:12:42 -0400

    update

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 class Bot(commands.Bot):
 
 	def __init__(self, *args, **kwargs):
-		super().__init__(command_prefix="s!", *args, **kwargs)
+		super().__init__(command_prefix="_", *args, **kwargs)
 		self.token = os.environ["TOKEN"]
 	
 	def run(self):
