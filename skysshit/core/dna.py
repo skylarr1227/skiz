1d0ce125c5437bc5771d3cecb8ab9aada85fcbae
Dy Lee <36546002+Dyleee@users.noreply.github.com>
2019-08-27 20:50:14 +0100

    Update dna.py

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 class Bot(commands.Bot):
 
 	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
+		super().__init__(command_prefix="s!", *args, **kwargs)
 	
 	def run(self):
 		super().run()
