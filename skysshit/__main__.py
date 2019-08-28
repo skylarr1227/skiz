0e1f19df3be247c2ebff42060b9861eb277bc20d
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-27 23:30:15 -0400

    Update __main__.py

--- skysshit/__main__.py
+++ skysshit/__main__.py
@@ -3,8 +3,7 @@ import discord
 from discord.ext import commands
 import aiohttp
 import re
-from datetime 
-import timedelta
+from datetime import timedelta
 import traceback
 import os
 from random import choice, randint
