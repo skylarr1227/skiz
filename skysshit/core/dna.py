04193626fa80d5c256c470d25ed85b6bbafe44ea
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-28 19:25:23 -0400

    Update dna.py

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -1,24 +1,21 @@
 import os
-import discord as discord
+import discord
 from discord.ext import commands
 import aiohttp
-import re
 from datetime import timedelta
-import traceback
-import os
 from random import choice, randint
 
 
 
 class Bot(commands.Bot):
 
-#@bot.event
-#async def on_ready():
-#    print('Logged in as')
- #   print(bot.user.name)
-#    print(bot.user.id)
-#    print('------')
-#    print(discord.utils.oauth_url(bot.user.id))
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    print(discord.utils.oauth_url(bot.user.id))
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(command_prefix="_", *args, **kwargs)
