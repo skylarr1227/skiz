4b690140eb7a2cc00f3669d1b7c4eb19ff32b1b6
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-27 23:27:26 -0400

    update to __main__.py

--- skysshit/__main__.py
+++ skysshit/__main__.py
@@ -1,3 +1,20 @@
 from .core.dna import Bot
+import discord
+from discord.ext import commands
+import aiohttp
+import re
+from datetime 
+import timedelta
+import traceback
+import os
+from random import choice, randint
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    print(discord.utils.oauth_url(bot.user.id))
 
 Bot().run()
