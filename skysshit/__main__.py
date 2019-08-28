59d0f992ddce032d945bde85aa409c7f4f014343
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-28 09:02:07 -0400

    Update __main__.py

--- skysshit/__main__.py
+++ skysshit/__main__.py
@@ -16,15 +16,15 @@ from random import choice, randint
 #    print('------')
 #    print(discord.utils.oauth_url(bot.user.id))
 
-@bot.command () 
-async def load ( extension_name : str ): 
-    """ Loads an extension. """ 
-    try : 
-        bot.load_extension(extension_name) 
-    except ( AttributeError , ImportError ) as e: 
-        await bot.say( " ```py \n {} : {} \n ``` " .format( type (e). __name__ , str (e))) 
-    return 
-    await bot.say( " {} loaded. " .format(extension_name)) 
+#@bot.command () 
+#async def load ( extension_name : str ): 
+#    """ Loads an extension. """ 
+#    try : 
+#        bot.load_extension(extension_name) 
+#    except ( AttributeError , ImportError ) as e: 
+#        await bot.say( " ```py \n {} : {} \n ``` " .format( type (e). __name__ , str (e))) 
+#    return 
+#    await bot.say( " {} loaded. " .format(extension_name)) 
  
 
 
