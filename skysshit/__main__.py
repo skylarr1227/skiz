74c53b1ae5434e5a2f8a14839ee3644b4083edeb
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-28 08:01:33 -0400

    Update __main__.py

--- skysshit/__main__.py
+++ skysshit/__main__.py
@@ -18,18 +18,14 @@ from random import choice, randint
 
 @bot.command () 
 async def load ( extension_name : str ): 
-""" Loads an extension. """ 
-try : 
-bot.load_extension(extension_name) 
-except ( AttributeError , ImportError ) as e: 
-await bot.say( " ```py \n {} : {} \n ``` " .format( type (e). __name__ , str (e))) 
-return 
-await bot.say( " {} loaded. " .format(extension_name)) 
+    """ Loads an extension. """ 
+    try : 
+        bot.load_extension(extension_name) 
+    except ( AttributeError , ImportError ) as e: 
+        await bot.say( " ```py \n {} : {} \n ``` " .format( type (e). __name__ , str (e))) 
+    return 
+    await bot.say( " {} loaded. " .format(extension_name)) 
  
-@bot.command () 
-async def unload ( extension_name : str ): 
- """ Unloads an extension. """ 
-bot.unload_extension(extension_name) 
-await bot.say( " {} unloaded. " .format(extension_name)) 
+
 
 Bot().run()
