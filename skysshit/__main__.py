93a898f802e6a13af0bce67ced89703fe5f18548
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-28 20:35:44 -0400

    Update __main__.py

--- skysshit/__main__.py
+++ skysshit/__main__.py
@@ -6,19 +6,18 @@ import discord
 from discord.ext import commands
 
 
-
+Bot = self.bot() 
 
 TOKEN = os.environ['TOKEN']
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
-    super().__init__(command_prefix="_", *args, **kwargs)
-    self.token = os.environ["TOKEN"]
-    self.skybot_cogs = os.listdir("skysshit/cogs")
+        super().__init__(command_prefix="_", *args, **kwargs)
+        self.token = os.environ["TOKEN"]
+        self.skybot_cogs = os.listdir("skysshit/cogs")
 	
 
 #bot = self.bot()
-Bot = self.bot() 
 
 #@bot.event
 #async def on_ready(self):
