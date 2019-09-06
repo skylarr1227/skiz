cebb95556b1129f8b68f51fb0895fe23e540ad05
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-06 02:10:24 -0400

    Update libneko.py

--- skysshit/cogs/libneko.py
+++ skysshit/cogs/libneko.py
@@ -2,25 +2,20 @@ import discord
 import libneko
 from discord.ext import commands
 from libneko import pag
-
 # Read the dummy text in.
 
 class Libneko(commands.Cog):
     #def __init__(self)
-    
+    #def __init__(self)
+    with open('dummy-text.txt') as fp:
+    dummy_text = fp.read()
+
     @commands.command()
     async def test(ctx):
-        try:
-            with open('dummy-text.txt') as fp:
-                dummy_text = fp.read()
-        except:
-            await ctx.send("Could not read")
         """We will be coding in here in the next part."""
         nav = pag.StringNavigatorFactory(max_lines=10)
-
         # Insert the dummy text into our factory.
         nav += dummy_text
-
         nav.start(ctx)
 
 def setup(bot):
