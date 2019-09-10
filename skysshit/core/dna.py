e353949947063cb81a17f38da030221dae540cbf
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-10 01:37:23 -0400

    Update dna.py

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -27,11 +27,11 @@ class Bot(commands.Bot):
 		super().__init__(command_prefix="_", *args, **kwargs)
 		self.token = os.environ["TOKEN"]
 		self.skybot_cogs = [ext for ext in os.listdir("skysshit/cogs") if ext.endswith(".py")]
-	        self.session = aiohttp.ClientSession(loop=self.loop, headers={"User-Agent"=self.http.user_agent})
-                self.browser_page = None
-                self.browser = self.loop.create_task(self.create_browser())
-                self.priv = self.config['extras'].get('privatebin', 'https://privatebin.net')
-                self.polr = self.config['extras'].get('polr', None)
+	       # self.session = aiohttp.ClientSession(loop=self.loop, headers={"User-Agent"=self.http.user_agent)
+                #self.browser_page = None
+             #   self.browser = self.loop.create_task(self.create_browser())
+               # self.priv = self.config['extras'].get('privatebin', 'https://privatebin.net')
+               # self.polr = self.config['extras'].get('polr', None)
 
                 self.commands_used = Counter()
                 self.commands_used_in = Counter()
