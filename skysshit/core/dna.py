57e29a5ef6d58a354a311b0d2d6b444adde95f0d
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-10 22:36:58 -0400

    Update dna.py

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -26,7 +26,7 @@ class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(command_prefix="_", *args, **kwargs)
         self.token = os.environ["TOKEN"]
-        bot.remove_command('help')
+        #bot.remove_command('help')
         self.skybot_cogs = [ext for ext in os.listdir("skysshit/cogs") if ext.endswith(".py")]
 	       # self.session = aiohttp.ClientSession(loop=self.loop, headers={"User-Agent"=self.http.user_agent)
                 #self.browser_page = None
