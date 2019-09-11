282fe67dc4ae943555e9eba9312511d37fb438b7
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-11 16:14:36 -0400

    Update dna.py

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -28,22 +28,22 @@ class Bot(commands.Bot):
         self.token = os.environ["TOKEN"]
         #bot.remove_command('help')
         self.skybot_cogs = [ext for ext in os.listdir("skysshit/cogs") if ext.endswith(".py")]
-	self.session = aiohttp.ClientSession(loop=self.loop, headers={"User-Agent"=self.http.user_agent)
+	self.session = aiohttp.ClientSession(loop=self.loop, headers={"User-Agent": self.http.user_agent})
         self.browser_page = None
-        self.browser = self.loop.create_task(self.create_browser())
+    self.browser = self.loop.create_task(self.create_browser())
        # self.priv = self.config['extras'].get('privatebin', 'https://privatebin.net')
       #  self.polr = self.config['extras'].get('polr', None)
 #
-        self.commands_used = Counter()
-        self.commands_used_in = Counter()
-        self.errors = deque(maxlen=10)
-        self.revisions = None
+self.commands_used = Counter()
+self.commands_used_in = Counter()
+self.errors = deque(maxlen=10)
+self.revisions = None
 
-        discord_logger = setup_logger("discord")
-        self.logger = setup_logger("Bot")
-        self.command_logger = setup_logger("Commands")
-        self.loggers = [discord_logger, self.logger, self.command_logger]
-        if 'bare' in kwargs.pop('argv'): 
+discord_logger = setup_logger("discord")
+self.logger = setup_logger("Bot")
+self.command_logger = setup_logger("Commands")
+self.loggers = [discord_logger, self.logger, self.command_logger]
+if 'bare' in kwargs.pop('argv'): 
       
 
         # make sure to only print ready text once
@@ -61,15 +61,15 @@ class Bot(commands.Bot):
                 self.logger.info(f"Bot started in {end_time} seconds")
                 self._loaded = True
 		print("Ready!")
-           await self.load_extensions()
+await self.load_extensions()
 
-        async def create_browser(self):
+async def create_browser(self):
             """Task to create browser for scraping purposes."""
             await self.wait_until_ready()
             self.browser = await launch(args=["--no-sandbox"], headless=True)
             self.browser_page = await self.browser.newPage()
             
-        async def load_extensions(self):
+async def load_extensions(self):
             for ext in self.skybot_cogs:
                 print("Loading %s" % ext)
                 self.load_extension(f"skysshit.cogs.{ext[:-3]}")
@@ -79,5 +79,5 @@ class Bot(commands.Bot):
 
 
         
-            def run(self):
+def run(self):
                 super().run(self.token)
