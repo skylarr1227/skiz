462e9ec17687ab7548cf0070a49fa1084e7993d3
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-10 22:33:46 -0400

    Update dna.py

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from pyppeteer import launch, errors
 
 from skysshit.utils.logging import setup_logger
-from skysshit.utils.over import send
+#from skysshit.utils.over import send
 
 
 discord.abc.Messageable.send = send
