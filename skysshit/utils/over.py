90a8fa148c58d1c2aabf8af3b0c8d663ca31c5db
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-11 23:25:48 -0400

    Update over.py

--- skysshit/utils/over.py
+++ skysshit/utils/over.py
@@ -6,7 +6,7 @@ import io
 import itertools
 
 import discord
-from discord.ext.commands import Bot, HelpFormatter
+#from discord.ext.commands import Bot, HelpFormatter
 from skysshit.utils import polr, privatebin
 from skysshit.utils.args import ArgParseConverter as ArgPC
 
