e10bc788c17083b603d0d31921618111fc5365cc
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-11 20:43:37 -0400

    Update dna.py

--- skysshit/core/dna.py
+++ skysshit/core/dna.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import aiohttp
 from datetime import timedelta
 from random import choice, randint
-import libneko
+#import libneko
 from discord import Embed
 from discord.ext.commands import is_owner
 
