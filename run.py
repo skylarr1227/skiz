e5ea6389775bb72f751df8c66da727165a135272
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-11 23:27:39 -0400

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import sys
 from ruamel import yaml
 
 from skysshit.core import dna
-from skysshit.utils.over import HelpFormatter
+#from skysshit.utils.over import HelpFormatter
 
 if not sys.version_info >= (3, 6):
     raise RuntimeError("Python 3.6+ not detected! Please run with 3.6 or above.")
