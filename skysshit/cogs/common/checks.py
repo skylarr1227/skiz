45772bf1e876a6755b836effddeea34b9ea1748e
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-13 15:11:10 -0400

    Create checks.py

---
+++ skysshit/cogs/common/checks.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+from discord.ext.commands import errors
+
+def bot_needs(perms):
+    def _(ctx):
+        me = ctx.guild.me if ctx.message.guild else ctx.bot.user
+        for perm in perms:
+            if not getattr(ctx.channel.permissions_for(me), perm):
+                raise errors.CheckFailure("I need the %s permission" % perm)
+        return True
+    return commands.check(_)
+
+def author_needs(perms):
+    def _(ctx):
+        author = ctx.message.author
+        for perm in perms:
+            if not getattr(ctx.channel.permissions_for(author), perm):
+                raise errors.CheckFailure("You need the %s permission" % perm)
+        return True
+    return commands.check(_)
+
+def is_owner():
+    def _(ctx):
+        if ctx.author.id != 86607397321207808:
+            raise errors.CheckFailure("I don't think you can do that...")
+        return True
+    return commands.check(_)
