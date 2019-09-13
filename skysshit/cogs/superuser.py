45263db29894bed8c57757e5dd87a5dd9d669e81
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-13 08:05:34 -0400

    Update superuser.py

--- skysshit/cogs/superuser.py
+++ skysshit/cogs/superuser.py
@@ -5,7 +5,7 @@ class WhitelistedOwnerSuperuserCog(superuser.SuperuserCog):
     
     owners = [1856271517491511531, 4325824835683126527, 21512642468346835632]
     async def owner_check(self, ctx):
-    return ctx.author.id in self.owners
+        return ctx.author.id in self.owners
     
     
     bot.add_cog(WhitelistedOwnerSuperuserCog())
