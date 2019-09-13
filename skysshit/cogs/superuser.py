1a0726feebb78d6882d26ab906bb4cd4e3f61167
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-13 08:09:12 -0400

    Update superuser.py

--- skysshit/cogs/superuser.py
+++ skysshit/cogs/superuser.py
@@ -2,11 +2,11 @@ from libneko.extras import superuser
 import discord
 from discord.ext import commands
 
-class WhitelistedOwnerSuperuserCog(commands.Cog):
+class WhitelistedOwnerSuperuserCog(superuser.SuperuserCog):
     
     owners = [1856271517491511531, 4325824835683126527, 21512642468346835632]
     async def owner_check(self, ctx):
         return ctx.author.id in self.owners
     
-    
+def setup(bot):
     bot.add_cog(WhitelistedOwnerSuperuserCog())
