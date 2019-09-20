60ef280f80da2cb28b1d49b71715945aac392ba5
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-20 06:14:09 -0400

    Update __main__.py

--- skysshit/__main__.py
+++ skysshit/__main__.py
@@ -1,4 +1,23 @@
 # Bruh
+import discord
 from .utils import checks
 from .core.dna import Bot
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('avatar'):
+        if len(message.mentions) > 0:
+            images = ''
+            for user in message.mentions:
+                images += str(user.avatar_url) + str('\n')
+            await message.channel.send(images)
+        else:
+            await message.channel.send(message.author.avatar_url)
+
 Bot().run()
