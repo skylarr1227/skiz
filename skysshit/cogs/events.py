2fe92680859af06bfcc2f416951ea63bf5df61f0
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-20 15:57:25 -0400

    Update events.py

--- skysshit/cogs/events.py
+++ skysshit/cogs/events.py
@@ -15,8 +15,8 @@ class Events(commands.Cog):
     async def on_message(self, message):
         if (message.mentions.__len__()>0):
             for user in message.mentions:
-            embed = discord.Embed(title="Users Avatar", description="Mentioned users Avatar", color=0x0084FD)
-            embed.set_image(url=user.avatar_url)
+                embed = discord.Embed(title="Users Avatar", description="Mentioned users Avatar", color=0x0084FD)
+                embed.set_image(url=user.avatar_url)
                 await ctx.send(embed=embed)
 
 def setup(bot):
