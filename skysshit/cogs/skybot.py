bfd09e5719fbe00323d52ab67d1ca57757fc650b
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-19 23:58:57 -0400

    Update skybot.py

--- skysshit/cogs/skybot.py
+++ skysshit/cogs/skybot.py
@@ -3,7 +3,10 @@ from libneko import pag
 
 with open('skysshit/cogs/dummy-text.txt') as fp:
     dummy_text = fp.read()
+with open('skysshit/cogs/plancan.txt') as file1:
+    plancan = file1.read()
 
+plancan_gif = 'http://157.245.8.88:8000/55ef51a74f091dad52a9bca3bccfb6cb.png'
 
 class Skybot(commands.Cog):
 	
@@ -13,6 +16,19 @@ class Skybot(commands.Cog):
             nav = pag.EmbedNavigatorFactory(max_lines=10)
             nav += dummy_text
             nav.start(ctx)
+
+        @commands.command()
+        @pag.embed_generator(max_chars=2048)
+        async def plancan(self, ctx):
+            """Rules and information regarding PlanCan"""
+            def plancan_embed(paginator, page, page_index):
+            embed = discord.Embed(colour=0xbfff00, description=page)
+            embed.set_image(url=plancan_gif)
+            return embed
+            nav = pag.EmbedNavigatorFactory(max_lines=10)
+            nav += plancan
+            nav.start(ctx)
+
 
 def setup(bot):
         bot.add_cog(Skybot())
